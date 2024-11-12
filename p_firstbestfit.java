a2860e9e63d8bb31bcfb3e66d899be5516537d24
prasad123123 <153524650+prasad123123@users.noreply.github.com>
2024-11-12 09:22:41 +0530

    Update p_firstbestfit.java

--- p_firstbestfit.java
+++ p_firstbestfit.java
@@ -1,111 +1,100 @@
-import java.util.*;
-public class p_firstbestfit{
+import java.util.Scanner;
 
-    public static void firstfit(int[] m_blk, int[] p_sz){
+public class  p_firstbestfit { 
+
+
+
+    public static void firstFit(int[] memoryBlocks, int[] processSizes) {
         System.out.println("\nFirst Fit Allocation:");
-        System.out.print("\nProcess | Size | Block Allocated | Remaining \n"); 
-   
-        for(int i=0;i<p_sz.length;i++){
-            int allow_blk=-1;
-            for(int j =0;j<m_blk.length;j++){
-                if(m_blk[j]>=p_sz[i]){
-                    allow_blk=j;
-    
-                    
+        System.out.printf("%-10s %-10s %-15s %-15s\n", "Process", "Size", "Block Allocated", "Remaining");
+
+        for (int i = 0; i < processSizes.length; i++) {
+            int allocatedBlock = -1;
+            for (int j = 0; j < memoryBlocks.length; j++) {
+                if (memoryBlocks[j] >= processSizes[i]) {
+                    allocatedBlock = j;
+                    memoryBlocks[j] -= processSizes[i];
                     break;
                 }
             }
-            if(allow_blk!=-1){
-                System.out.printf("\n%8d | %4d | %15d | %15d \n",i+1,p_sz[i],m_blk[allow_blk],m_blk[allow_blk] -= p_sz[i]);
-                m_blk[allow_blk] -= p_sz[i];
-                
-            }
-            else{
-                System.out.printf("\n%8d | %4d | %15s | %15s \n",i+1,p_sz[i],"Not Allocated","-");
+
+            if (allocatedBlock != -1) {
+                System.out.printf("%-10d %-10d %-15d %-15d\n", i + 1, processSizes[i], allocatedBlock + 1, memoryBlocks[allocatedBlock]);
+            } else {
+                System.out.printf("%-10d %-10d %-15s %-15s\n", i + 1, processSizes[i], "Not Allocated", "-");
             }
         }
-
-        
-        
     }
-    
 
-    public static void best(int[] m_blk, int[] p_sz){
-        System.out.println("\nFirst Fit Allocation:");
-        System.out.print("\nProcess | Size | Block Allocated | Remaining \n"); 
-   
-        for(int i=0;i<p_sz.length;i++){
-            int allow_blk=-1;
-            int min_space=Integer.MAX_VALUE;
-            for(int j =0;j<m_blk.length;j++){
-                int r_space=m_blk[j]-p_sz[i];
+    public static void bestFit(int[] memoryBlocks, int[] processSizes) {
+        System.out.println("\nBest Fit Allocation:");
+        System.out.printf("%-10s %-10s %-15s %-15s\n", "Process", "Size", "Block Allocated", "Remaining");
 
-                if(r_space>=0 && r_space<min_space){
-                    min_space=r_space;
-                    allow_blk=j;
-                    
+        for (int i = 0; i < processSizes.length; i++) {
+            int allocatedBlock = -1;
+            int minRemainingSpace = Integer.MAX_VALUE;
+
+            for (int j = 0; j < memoryBlocks.length; j++) {
+                int remainingSpace = memoryBlocks[j] - processSizes[i];
+                if (remainingSpace >= 0 && remainingSpace < minRemainingSpace) {
+                    minRemainingSpace = remainingSpace;
+                    allocatedBlock = j;
                 }
             }
-            if(allow_blk!=-1){
-                
-                System.out.printf("\n%8d | %4d | %15d | %15d \n",i+1,p_sz[i],m_blk[allow_blk],m_blk[allow_blk] -= p_sz[i]);
-                m_blk[allow_blk] -= p_sz[i];
-            }
-            else{
-                System.out.printf("\n%8d | %4d | %15s | %15s \n",i+1,p_sz[i],"Not Allocated","-");
+
+            if (allocatedBlock != -1) {
+                memoryBlocks[allocatedBlock] -= processSizes[i];
+                System.out.printf("%-10d %-10d %-15d %-15d\n", i + 1, processSizes[i], allocatedBlock + 1, memoryBlocks[allocatedBlock]);
+            } else {
+                System.out.printf("%-10d %-10d %-15s %-15s\n", i + 1, processSizes[i], "Not Allocated", "-");
             }
         }
     }
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number of Memory Blocks: ");
-        int n_blk = sc.nextInt();
-        int[] m_blk= new int[n_blk];
-        System.out.println("Enter the size of each Memory Block: ");
-        for(int i=0;i<n_blk;i++){
-            System.out.print("Block "+(i+1)+": ");
-            m_blk[i]=sc.nextInt();
+        // Input memory blocks
+        System.out.print("Enter the number of memory blocks: ");
+        int blockCount = scanner.nextInt();
+        int[] memoryBlocks = new int[blockCount];
+        System.out.println("Enter the size of each memory block:");
+        for (int i = 0; i < blockCount; i++) {
+            System.out.print("Block " + (i + 1) + " size: ");
+            memoryBlocks[i] = scanner.nextInt();
         }
 
-        System.out.println("Enter the number of processes: ");
-        int n_prs = sc.nextInt();
-        int[] p_sz= new int[n_prs];
-        System.out.println("Enter the size of each process: ");
-        for(int i=0;i<n_prs;i++){
-            System.out.print("process "+(i+1)+": ");
-            p_sz[i]=sc.nextInt();
+        // Input process sizes
+        System.out.print("Enter the number of processes: ");
+        int processCount = scanner.nextInt();
+        int[] processSizes = new int[processCount];
+        System.out.println("Enter the size of each process:");
+        for (int i = 0; i < processCount; i++) {
+            System.out.print("Process " + (i + 1) + " size: ");
+            processSizes[i] = scanner.nextInt();
         }
-        int ch;
 
-        do{
+        while (true) {
             System.out.println("\nMemory Allocation Strategies:");
             System.out.println("1. First Fit");
             System.out.println("2. Best Fit");
             System.out.println("3. Exit");
-            System.out.print("Enter Choice: ");
-            ch= sc.nextInt();
-            switch (ch) {
+            System.out.print("Select a strategy: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
                 case 1:
-                    firstfit(m_blk.clone(), p_sz);
+                    firstFit(memoryBlocks.clone(), processSizes);
                     break;
                 case 2:
-                    best(m_blk.clone(), p_sz);
+                    bestFit(memoryBlocks.clone(), processSizes);
                     break;
                 case 3:
                     System.out.println("Exiting program.");
-                    sc.close();
+                    scanner.close();
                     return;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
-
-        }while(ch!=3);
-
-
-
-
+        }
     }
-
 }
